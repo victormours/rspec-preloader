a0c4c6f2fbd95687240d37d5a3f9048d2d45bd04
Victor Mours <victor.mours@gmail.com>
2014-05-25 13:07:59 +0200

    Add failing test for loading source file

---
+++ spec/fake_project/app/some_class.rb
@@ -0,0 +1,1 @@
+Loading source file
--- spec/rspec_preloader_spec.rb
+++ spec/rspec_preloader_spec.rb
@@ -14,7 +14,20 @@ class TestRspecPreloader < Minitest::Test
     assert_equal "Loading spec helper", preloader_output[1]
   end
 
+  def test_changed_source_file_is_loaded
+    source_filename = File.expand_path("fake_project/app/some_class.rb", __dir__)
+
+    File.open(source_filename, 'w') do |file|
+      file.puts "Loading source file"
+
+    end
+
+    assert_equal "Loading source file", preloader_output[2]
+
+    File.delete(source_filename)
+  end
+
   def test_specs_are_run
-    assert_equal  "Running specs", preloader_output[2]
+    assert_equal  "Running specs", preloader_output[3]
   end
 end
