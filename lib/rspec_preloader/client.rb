18af90e23cecbee72c14520fe5929c22781d786e
Victor Mours <victor.mours@gmail.com>
2014-10-02 17:14:21 +0200

    Basic client/server working

--- lib/rspec_preloader/client.rb
+++ lib/rspec_preloader/client.rb
@@ -9,7 +9,7 @@ class RspecPreloader
       DRb.start_service
 
       rspec_runner = DRbObject.new_with_uri(SERVER_URI)
-      rspec_runner.run_rspec(rspec_arguments)
+      rspec_runner.run_rspec(rspec_arguments, STDERR, STDOUT)
     end
 
   end
